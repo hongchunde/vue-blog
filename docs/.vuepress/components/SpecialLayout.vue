1f3c0a70aee9da6522805484c17e2c495562979b
hongchunde@qq.com <hongchunde@qq.com>
2018-11-11 01:05:31 +0800

    master

---
+++ docs/.vuepress/components/SpecialLayout.vue
@@ -0,0 +1,62 @@
+<template>
+<div id="page">
+	<div class="theme-container no-sidebar">
+		<div class="home">
+			<div class="hero">
+				<!---->
+				<h1>Tom</h1>
+				<p class="description">
+                      Hello, World
+				</p>
+				<p class="action">
+					<a href="/baseComponents/" class="nav-link action-button">前往 →</a>
+				</p>
+			</div>
+			<div class="features">
+				<div class="feature">
+					<h2>编程技术专题</h2>
+					<p>
+						操作系统，消息中间件，缓存中间件，数据库系统，WEB后端框架，HTML5开发，前端框架，分布式容器
+					</p>
+				</div>
+				<div class="feature">
+					<h2>编程开发工具</h2>
+					<p>
+						开发集成IDE，代码托管服务器，源码编辑器，反编译工具
+					</p>
+				</div>
+				<div class="feature">
+					<h2>服务器部署</h2>
+					<p>
+						阿里云服务器，ECS，域名注册，解析，备案
+					</p>
+				</div>
+				<div class="feature">
+					<h2>在线工具箱</h2>
+					<p>
+						json，XML,HTML格式化，印象笔记，禅道，confluence,xwiki
+					</p>
+				</div>
+				<div class="feature">
+					<h2>工作简历</h2>
+					<p>
+						华南农业大学（软件工程），北京中软国际，南京中兴软创，立信诚毅科技，卓望数码
+					</p>
+				</div>
+				<div class="feature">
+					<h2>生活专题</h2>
+					<p>
+						音乐学习（吉他、钢琴，歌唱），小说阅读，影视专题（TVB），旅游分享（国内，国外），运动健康，篮球
+					</p>
+				</div>
+			</div>
+			<div class="content custom">
+			</div>
+			<div class="footer">
+                    MIT Licensed | Copyright © 2018-present <a href="http://www.iamtom.info"> tom</a>
+			</div>
+		</div>
+		<!---->
+	</div>
+</div>
+</template>
